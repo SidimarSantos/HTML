9176b970c0b06e4a6a2260ec2dee8e47e0175329
Sidimar Santos <sidimarsantos827@gmail.com>
2024-07-27 20:14:16 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let palavra = 34.6;
+
+// alert(palavra.toUpperCase());
+// alert(palavra.toLowerCase());
+// alert(palavra.indexOf("a"));
+// alert(palavra.lastIndexOf("a"));
+//  alert(palavra.slice(10, 20));
+
+// alert(Math.max(-2, 5 , -4));
+
+// let numAleatorio = Math.random() * 1000;
+
+// alert(Math.round(numAleatorio));
+
+// function somar(){
+//     alert(n1 + n2);
+// }
+
+function acao(){
+    let texto = document.getElementsByTagName("h1")[0];
+    alert(texto.innerHTML);
+}
